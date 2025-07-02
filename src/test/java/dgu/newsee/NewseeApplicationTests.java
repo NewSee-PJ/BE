88b0408e0305b4744bc9c7dc6a620fa5c8c79b0d
haeun1107 <haeun9394@gmail.com>
2025-07-03 01:06:07 +0800

    [Feature] 프로젝트 초기 세팅

---
+++ src/test/java/dgu/newsee/NewseeApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.newsee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
