9b02771ff68041d13babd895483833929187b6b8
haeun1107 <haeun9394@gmail.com>
2025-07-09 16:47:47 +0800

    [Feature] #5 - custom User 인증 객체 생성

--- src/main/java/dgu/newsee/global/security/CustomUserDetails.java
+++ src/main/java/dgu/newsee/global/security/CustomUserDetails.java
@@ -1,4 +1,38 @@
 package dgu.newsee.global.security;
 
-public class CustomUserDetails {
+import dgu.newsee.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final String email;
+    private final String role;
+
+    public CustomUserDetails(User user) {
+        this.userId = user.getUserId();
+        this.email = user.getEmail();
+        this.role = user.getRole().name();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override public String getPassword() { return null; } // OAuth 기반이라 null
+    @Override public String getUsername() { return email; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
